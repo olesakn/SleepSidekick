6e3a05fc83d35c977801cb4535ce63d4dd6666d3
Nick Olesak <olesakn@mail.gvsu.edu>
2013-11-06 11:58:50 -0500

    Stuff

--- gen/com/cis368/sleepsidekick/R.java
+++ gen/com/cis368/sleepsidekick/R.java
@@ -10,6 +10,9 @@ package com.cis368.sleepsidekick;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int dark_gray=0x7f040000;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -17,18 +20,21 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080004;
-        public static final int home_analogClock=0x7f080001;
-        public static final int home_button_go_to_sleep=0x7f080002;
-        public static final int home_digitalClock=0x7f080000;
-        public static final int pager=0x7f080003;
+        public static final int Button01=0x7f090000;
+        public static final int action_settings=0x7f090007;
+        public static final int home_button_go_to_sleep=0x7f090002;
+        public static final int home_button_schedule=0x7f090001;
+        public static final int home_digitalClock=0x7f090003;
+        public static final int home_textview_date=0x7f090004;
+        public static final int pager=0x7f090006;
+        public static final int textView1=0x7f090005;
     }
     public static final class layout {
         public static final int fragment_alarms=0x7f030000;
@@ -37,14 +43,14 @@ public final class R {
         public static final int home=0x7f030003;
     }
     public static final class menu {
-        public static final int home=0x7f070000;
+        public static final int home=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int title_section1=0x7f050002;
-        public static final int title_section2=0x7f050003;
-        public static final int title_section3=0x7f050004;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int title_section1=0x7f060002;
+        public static final int title_section2=0x7f060003;
+        public static final int title_section3=0x7f060004;
     }
     public static final class style {
         /** 
@@ -68,10 +74,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
--- src/com/cis368/sleepsidekick/HomeActivity.java
+++ src/com/cis368/sleepsidekick/HomeActivity.java
@@ -4,7 +4,9 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class HomeActivity extends Fragment {
 
@@ -13,11 +15,19 @@ public class HomeActivity extends Fragment {
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		View rootView = inflater.inflate(R.layout.fragment_home, container, false);
 
+		
+		
+		
+		/*
 		// Build GUI here
-
-		
-		
-		
+		final Button goToSleepButton = (Button) getActivity().findViewById(R.id.home_button_go_to_sleep);
+		goToSleepButton.setOnClickListener(new OnClickListener() {
+			public void onClick(View arg0) {
+				goToSleepButton.setText("Corey is a losser");
+				
+			}
+		});
+		*/
 		return rootView;
 	}
 }
