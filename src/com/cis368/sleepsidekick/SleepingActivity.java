739047908e6dab891ea70dd4546170cd53b1e2ba
olesakn <olesakn@mail.gvsu.edu>
2013-11-17 15:07:54 -0500

    More work on sleep stuff

--- src/com/cis368/sleepsidekick/SleepingActivity.java
+++ src/com/cis368/sleepsidekick/SleepingActivity.java
@@ -1,18 +1,45 @@
 package com.cis368.sleepsidekick;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class SleepingActivity extends Activity {
+public class SleepingActivity extends Activity implements OnClickListener {
 
+	Button disableSA, disableAlarm;
 
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.sleeping);
+	
+		
+		disableSA = (Button) findViewById(R.id.sleeping_button_sleep_air);
+		disableSA.setOnClickListener(this);
+		
+		disableAlarm = (Button) findViewById(R.id.sleeping_button_alarm);
+		disableAlarm.setOnClickListener(this);
 		
 		
+		
+		// this is for testing purposes
+		// switches to "alarm going off" after 10 seconds
+		try {
+			Thread.sleep(10000);
+		} catch (InterruptedException e) {}
+		startActivity(new Intent(this, TaskMathProblemActivity.class));
+		finish();
 	}
-	
-	
 
+	public void onClick(View v) {
+		if (v == disableSA){
+			
+		}
+		if (v == disableAlarm) {
+			Intent i = new Intent(this, MainActivity.class);
+			
+		}
+	}
 }
